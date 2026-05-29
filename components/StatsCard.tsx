5adbd9af865ac639b738dc74831b0e03dd13a9b8
Ebrahim Ghaffari <evrin361@gmail.com>
2026-05-29 16:52:16 +0330

    fix statscard props

--- components/StatsCard.tsx
+++ components/StatsCard.tsx
@@ -1,4 +1,10 @@
-export default function StatsCard({ title, value }) {
+export default function StatsCard({
+  title,
+  value,
+}: {
+  title: string
+  value: string | number
+}) {
   return (
     <div className="bg-white/5 backdrop-blur-xl border border-white/10 rounded-[30px] p-6 shadow-[0_0_30px_rgba(0,0,0,0.5)]">
       <h3 className="text-yellow-400 font-black text-xl">{title}</h3>
